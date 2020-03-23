c499e20a6d5f379ff230a6d879ae4d549c5719a3
Mateo Torres <torresmateo@gmail.com>
2020-03-23 00:20:04 -0300

    updated app

--- app.py
+++ app.py
@@ -2,24 +2,25 @@ from flask import Flask, jsonify, request
 from datetime import datetime
 import os
 
+data = []
+skip = True
+for line in open('covidpy.csv')
+    if skip:
+        skip = False
+        continue
+    date, cases, deaths, tests, recoveries = line.split(',')
+    data.append({
+        'date':date, 
+        'cases':cases, 
+        'deaths':deaths, 
+        'tests':tests, 
+        'recoveries':recoveries
+    })
+
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    data = []
-    skip = True
-    for line in open('covidpy.csv')
-        if skip:
-            skip = False
-            continue
-        date, cases, deaths, tests, recoveries = line.split(',')
-        data.append({
-            'date':date, 
-            'cases':cases, 
-            'deaths':deaths, 
-            'tests':tests, 
-            'recoveries':recoveries
-        })
     return jsonify(data), 200
 
 # @app.route('/recharts')
