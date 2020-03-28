fbd7e3bc992bab062dc6cff594959d331f32ffce
Mateo Torres <torresmateo@gmail.com>
2020-03-27 21:14:35 -0300

    add a timer

--- app.py
+++ app.py
@@ -1,8 +1,37 @@
 from flask import Flask, request, send_from_directory
 from flask_cors import CORS, cross_origin
 from scripts import script as scripts
-scripts.run()
+from threading import Timer
 
+class RepeatedTimer(object):
+    def __init__(self, interval, function, *args, **kwargs):
+        self._timer     = None
+        self.interval   = interval
+        self.function   = function
+        self.args       = args
+        self.kwargs     = kwargs
+        self.is_running = False
+        self.start()
+
+    def _run(self):
+        self.is_running = False
+        self.start()
+        self.function(*self.args, **self.kwargs)
+
+    def start(self):
+        if not self.is_running:
+            self._timer = Timer(self.interval, self._run)
+            self._timer.start()
+            self.is_running = True
+
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False
+
+def update_stuff():
+    scripts.run()
+
+rt = RepeatedTimer(10 * 60, update_stuff)
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -16,4 +45,9 @@ def send_js(path):
 
 
 if __name__ == "__main__":
+    t = Timer(10 * 60, update_stuff)
+    t.start()
+    t.join()
     app.run()
+
+# zsh oh-my-zsh
